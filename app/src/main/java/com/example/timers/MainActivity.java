c0b64456fea715946a9610cbf291ea84741dded2
Moses Pinto <pinto.com@gmail.com>
2020-10-17 16:45:20 +0300

    Fixed rtl problem, added heart gesture

--- app/src/main/java/com/example/timers/MainActivity.java
+++ app/src/main/java/com/example/timers/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.IntentFilter;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.os.PowerManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.LocalBroadcastManager;
@@ -29,11 +30,84 @@ public class MainActivity extends AppCompatActivity {
     TextView minutes_tv;
     TextView seconds_tv;
     TextView button;
+    ImageView heart;
+    int volume_minimum = 60;
     static boolean counterIsActive = false ;
     CountDownTimer countDownTimer;
     MediaPlayer mPlayer;
     boolean mPlayerIsActive = false;
     int currentTimeLeft = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        minutes_tv = findViewById(R.id.Minutes);
+        seconds_tv = findViewById(R.id.Seconds);
+        fireGif = findViewById(R.id.fire);
+        seekbar = findViewById(R.id.seekBar);
+        button = findViewById(R.id.start_button);
+        heart = findViewById(R.id.heart);
+        seekbar.setMax(1200);
+        seekbar.setProgress(30);
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                updateTimeBar(i);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        Glide.with(this)
+                .load(R.drawable.fire)
+                .into(fireGif);
+        fireGif.animate().alpha(0f).translationYBy(-10);
+
+        VolumeCheck();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        stopService();
+        controlTimerIfRestored();
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        if (counterIsActive){
+            counterIsActive = false;
+            countDownTimer.cancel();
+            startService();
+        }
+        if(mPlayerIsActive) {
+            mPlayer.stop();
+            mPlayerIsActive = false;
+        }
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        stopService();
+        super.onDestroy();
+    }
 
     public void VolumeCheck(){
         //Media volume levels check
@@ -52,7 +126,7 @@ public class MainActivity extends AppCompatActivity {
         int max = am.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         int volume_percentage =  (int)((((float)music_volume_level/(float)max)) * 100.0);
         Log.i("VolumeCheck", "volume_percentage: " + volume_percentage);
-        if (volume_percentage < 25){
+        if (volume_percentage < volume_minimum){
             Toast.makeText(getApplicationContext(),"Sound volume is too low (pump it up)", Toast.LENGTH_LONG).show();
         }
     }
@@ -110,7 +184,7 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             seconds_tv.setText(Integer.toString(seconds));
-            }
+        }
         if(minutes < 10){
             minutes_tv.setText("0" + Integer.toString(minutes));
         }
@@ -189,73 +263,18 @@ public class MainActivity extends AppCompatActivity {
         //---------------------------------------------------------
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        minutes_tv = findViewById(R.id.Minutes);
-        seconds_tv = findViewById(R.id.Seconds);
-        fireGif = findViewById(R.id.fire);
-        seekbar = findViewById(R.id.seekBar);
-        button = findViewById(R.id.start_button);
-        seekbar.setMax(1200);
-        seekbar.setProgress(30);
-        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                updateTimeBar(i);
-            }
+    public void showHeart(View view){
+        new Handler().post(new Runnable() {
 
             @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
+            public void run() {
+                heart.setScaleX(0f);
+                heart.setScaleY(0f);
+                heart.setVisibility(View.VISIBLE);
+                heart.setAlpha(1f);
+                heart.animate().alpha(1f).scaleXBy(1f).scaleYBy(1f).alpha(0f).setDuration(1500);
             }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-        Glide.with(this)
-                .load(R.drawable.fire)
-                .into(fireGif);
-        fireGif.animate().alpha(0f).translationYBy(-10);
-
-        VolumeCheck();
+        } );
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-    }
-    @Override
-    protected void onResume() {
-        stopService();
-        controlTimerIfRestored();
-        super.onResume();
-    }
-    @Override
-    protected void onPause() {
-        if (counterIsActive){
-            counterIsActive = false;
-            countDownTimer.cancel();
-            startService();
-        }
-        if(mPlayerIsActive) {
-            mPlayer.stop();
-            mPlayerIsActive = false;
-        }
-        LocalBroadcastManager.getInstance(this).unregisterReceiver(mMessageReceiver);
-        super.onPause();
-    }
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-    @Override
-    protected void onDestroy() {
-        stopService();
-        super.onDestroy();
-    }
 }
---
+++ app/src/main/java/com/example/timers/MyTime.java
@@ -0,0 +1,4 @@
+package com.example.timers;
+
+public class MyTime {
+}
